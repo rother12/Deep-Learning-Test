0b5efe398e352aae6bed9355b191cbf77f28c51e
WonSikJung <rother12@hanmail.net>
2023-03-09 00:45:40 +0000

    Add TEST3

---
+++ CNN_LSTM_Part2_DataLoader & ML Train.py
@@ -0,0 +1,387 @@
+import os
+import nltk
+import pickle
+import numpy as np
+from PIL import Image
+from collections import Counter
+from pycocotools.coco import COCO
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+from torchvision import transforms
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.nn.utils.rnn import pack_padded_sequence
+
+
+##Main Module
+import nltk
+from pycocotools.coco import COCO
+import torch.utils.data as data
+import torchvision.models as model
+import torchvision.transforms as transform
+from torch.nn.utils.rnn import pack_padded_sequence
+from pprint import pprint as pp
+
+
+__version__=['0.0.1']
+__author__ = 'Won Sik Jung <rother12@hanmail.net>'
+
+'''Reference is '파이토치 딥러닝 프로젝트(2022.02) 
+This Code for Ai Test & Train
+CV(Computer Vision) Chapter 2.__ CNN-LSTM Model
+
+CNN content Vector Change Input Vector
+
+'''
+
+
+
+# 1) = Data Processing
+    # 1-1) Data Processing, Build Vocab
+    # 1-2) Data Processing, Resize Shape
+
+
+# 2) = Data Loader, Training
+    # 2-1) Data Loader Definition
+    # 2-2) Model Definintion
+
+# 3) = Training Process
+#     3-1) Training
+        # 3-1-1) Training setting
+        # 3-1-2) Training
+#     3-2) Prediction Caption
+
+Basic_path = '/home/wonsik.jung/coco_dataset/'
+
+
+# 2-1) Data Loader Definition
+
+class CustomCocoDataset(data.Dataset):
+    """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
+    def __init__(self, data_path, coco_json_path, vocabulary, transform=None):
+        """Set the path for images, captions and vocabulary wrapper.
+        
+        Args:
+            root: image directory.
+            json: coco annotation file path.
+            vocab: vocabulary wrapper.
+            transform: image transformer.
+        """
+        self.root = data_path
+        self.coco_data = COCO(coco_json_path)
+        self.indices = list(self.coco_data.anns.keys())
+        self.vocabulary = vocabulary
+        self.transform = transform
+ 
+    def __getitem__(self, idx):
+        """Returns one data pair (image and caption)."""
+        coco_data = self.coco_data
+        vocabulary = self.vocabulary
+        annotation_id = self.indices[idx]
+        caption = coco_data.anns[annotation_id]['caption']
+        image_id = coco_data.anns[annotation_id]['image_id']
+        image_path = coco_data.loadImgs(image_id)[0]['file_name']
+ 
+        image = Image.open(os.path.join(self.root, image_path)).convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+ 
+        # Convert caption (string) to word ids.
+        word_tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+        caption = []
+        caption.append(vocabulary('<start>'))
+        caption.extend([vocabulary(token) for token in word_tokens])
+        caption.append(vocabulary('<end>'))
+        ground_truth = torch.Tensor(caption)
+        return image, ground_truth
+ 
+    def __len__(self):
+        return len(self.indices)
+ 
+ 
+def collate_function(data_batch):
+    """Creates mini-batch tensors from the list of tuples (image, caption).
+    
+    We should build custom collate_fn rather than using default collate_fn, 
+    because merging caption (including padding) is not supported in default.
+    Args:
+        data: list of tuple (image, caption). 
+            - image: torch tensor of shape (3, 256, 256).
+            - caption: torch tensor of shape (?); variable length.
+    Returns:
+        images: torch tensor of shape (batch_size, 3, 256, 256).
+        targets: torch tensor of shape (batch_size, padded_length).
+        lengths: list; valid length for each padded caption.
+    """
+    # Sort a data list by caption length (descending order).
+    data_batch.sort(key=lambda d: len(d[1]), reverse=True)
+    imgs, caps = zip(*data_batch)
+
+    # Merge images (from list of 3D tensors to 4D tensor).
+    # Originally, imgs is a list of <batch_size> number of RGB images with dimensions (3, 256, 256)
+    # This line of code turns it into a single tensor of dimensions (<batch_size>, 3, 256, 256)
+    imgs = torch.stack(imgs, 0)
+ 
+    # Merge captions (from list of 1D tensors to 2D tensor), similar to merging of images donw above.
+    cap_lens = [len(cap) for cap in caps]
+    tgts = torch.zeros(len(caps), max(cap_lens)).long()
+    for i, cap in enumerate(caps):
+        end = cap_lens[i]
+        tgts[i, :end] = cap[:end]
+    return imgs, tgts, cap_lens
+ 
+def get_loader(data_path, coco_json_path, vocabulary, transform, batch_size, shuffle, num_workers):
+    """Returns torch.utils.data.DataLoader for custom coco dataset."""
+    # COCO caption dataset
+    coco_dataser = CustomCocoDataset(data_path=data_path,
+                       coco_json_path=coco_json_path,
+                       vocabulary=vocabulary,
+                       transform=transform)
+    
+    # Data loader for COCO dataset
+    # This will return (images, captions, lengths) for each iteration.
+    # images: a tensor of shape (batch_size, 3, 224, 224).
+    # captions: a tensor of shape (batch_size, padded_length).
+    # lengths: a list indicating valid length for each caption. length is (batch_size).
+    custom_data_loader = torch.utils.data.DataLoader(dataset=coco_dataser, 
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              num_workers=num_workers,
+                                              collate_fn=collate_function)
+    return custom_data_loader
+
+
+# 2-2) Model Definition
+
+
+class CNNModel(nn.Module):
+    def __init__(self, embedding_size):
+        """Load the pretrained ResNet-152 and replace top fc layer."""
+        super(CNNModel, self).__init__()
+
+        # resnet = models.resnet152(pretrained=True)
+        # Pretrained Option Make Deprecated Error,
+        
+        resnet = models.resnet152(weights = None)
+        module_list = list(resnet.children())[:-1]      # delete the last fc layer.
+        self.resnet_module = nn.Sequential(*module_list)
+        self.linear_layer = nn.Linear(resnet.fc.in_features, embedding_size)
+        self.batch_norm = nn.BatchNorm1d(embedding_size, momentum=0.01)
+        
+    def forward(self, input_images):
+        """Extract feature vectors from input images."""
+        with torch.no_grad():
+            resnet_features = self.resnet_module(input_images)
+        resnet_features = resnet_features.reshape(resnet_features.size(0), -1)
+        final_features = self.batch_norm(self.linear_layer(resnet_features))
+        return final_features
+ 
+ 
+class LSTMModel(nn.Module):
+    def __init__(self, embedding_size, hidden_layer_size, vocabulary_size, num_layers, max_seq_len=20):
+        """Set the hyper-parameters and build the layers."""
+        super(LSTMModel, self).__init__()
+        self.embedding_layer = nn.Embedding(vocabulary_size, embedding_size)
+        self.lstm_layer = nn.LSTM(embedding_size, hidden_layer_size, num_layers, batch_first=True)
+        self.linear_layer = nn.Linear(hidden_layer_size, vocabulary_size)
+        self.max_seq_len = max_seq_len
+        
+    def forward(self, input_features, capts, lens):
+        """Decode image feature vectors and generates captions."""
+        embeddings = self.embedding_layer(caps)
+        embeddings = torch.cat((input_features.unsqueeze(1), embeddings), 1)
+        lstm_input = pack_padded_sequence(embeddings, lens, batch_first=True) 
+        hidden_variables, _ = self.lstm_layer(lstm_input)
+        model_outputs = self.linear_layer(hidden_variables[0])
+        return model_outputs
+    
+    def sample(self, input_features, lstm_states=None):
+        """Generate captions for given image features using greedy search."""
+        sampled_indices = []
+        lstm_inputs = input_features.unsqueeze(1)
+        for i in range(self.max_seq_len):
+            hidden_variables, lstm_states = self.lstm_layer(lstm_inputs, lstm_states)          # hiddens: (batch_size, 1, hidden_size)
+            model_outputs = self.linear_layer(hidden_variables.squeeze(1))            # outputs:  (batch_size, vocab_size)
+            _, predicted_outputs = model_outputs.max(1)                        # predicted: (batch_size)
+            sampled_indices.append(predicted_outputs)
+            lstm_inputs = self.embedding_layer(predicted_outputs)                       # inputs: (batch_size, embed_size)
+            lstm_inputs = lstm_inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
+        sampled_indices = torch.stack(sampled_indices, 1)                # sampled_ids: (batch_size, max_seq_length)
+        return sampled_indices
+
+
+# 3-1-1) Training Setting
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# Create model directory
+
+if not os.path.exists(Basic_path):
+    os.makedirs(Basic_path)
+
+    
+# Image preprocessing, normalization for the pretrained resnet
+transform = transforms.Compose([ 
+    transforms.RandomCrop(224),
+    transforms.RandomHorizontalFlip(), 
+    transforms.ToTensor(), 
+    transforms.Normalize((0.485, 0.456, 0.406), 
+                         (0.229, 0.224, 0.225))])
+
+
+# Load vocabulary wrapper
+with open(Basic_path+'vocabulary.pkl', 'rb') as f:
+    Vocab = pickle.load(f)
+
+            
+# 3-1-2) Traing
+
+
+# Build data loader
+custom_data_loader = get_loader("/home/wonsik.jung/coco_dataset/cocodataset/resized_images", "/home/wonsik.jung/coco_dataset/annotations/captions_train2014.json", vocabulary, 
+                         transform, 128,
+                         shuffle=True, num_workers=0) 
+
+
+# Build the models
+encoder_model = CNNModel(256).to(device)
+decoder_model = LSTMModel(256, 512, len(vocabulary), 1).to(device)
+ 
+    
+# Loss and optimizer
+loss_criterion = nn.CrossEntropyLoss()
+parameters = list(decoder_model.parameters()) + list(encoder_model.linear_layer.parameters()) + list(encoder_model.batch_norm.parameters())
+optimizer = torch.optim.Adam(parameters, lr=0.001)
+
+
+# Train the models
+total_num_steps = len(custom_data_loader)
+for epoch in range(5):
+    for i, (imgs, caps, lens) in enumerate(custom_data_loader):
+ 
+        # Set mini-batch dataset
+        imgs = imgs.to(device)
+        caps = caps.to(device)
+        tgts = pack_padded_sequence(caps, lens, batch_first=True)[0]
+ 
+        # Forward, backward and optimize
+        feats = encoder_model(imgs)
+        outputs = decoder_model(feats, caps, lens)
+        loss = loss_criterion(outputs, tgts)
+        decoder_model.zero_grad()
+        encoder_model.zero_grad()
+        loss.backward()
+        optimizer.step()
+ 
+        # Print log info
+        if i % 10 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
+                  .format(epoch, 5, i, total_num_steps, loss.item(),
+                          np.exp(loss.item()))) 
+ 
+        # Save the model checkpoints
+
+        if (i+1) % 1000 == 0:
+
+            torch.save(decoder_model.state_dict(), os.path.join(
+                'home/wonsik.jung/coco_dataset/', 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+            torch.save(encoder_model.state_dict(), os.path.join(
+                'home/wonsik.jung/coco_dataset/', 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+            
+
+# Done, We Can Get Data state_dict information
+# Spend 30 min, GPU = RTX A 6000
+
+
+# Prediction Caption
+
+# first, I Recommend pick up, Some Sample picture in Your image_file_path, name as Sample.jpg
+
+# Our Sample_ Image_file
+image_file_path = Basic_path+"Sample.jpg"
+ 
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+ 
+
+def load_image(image_file_path, transform=None):
+    img = Image.open(image_file_path).convert('RGB')
+    # img = img.resize([224, 224], Image.LANCZOS)
+    # Old Version DepreCated Error
+    img = img.resize([224, 224], Image.Resampling.LANCZOS)
+
+    
+    if transform is not None:
+        img = transform(img).unsqueeze(0)
+    
+    return img
+ 
+
+# Image preprocessing
+transform = transforms.Compose([
+    transforms.ToTensor(), 
+    transforms.Normalize((0.485, 0.456, 0.406), 
+                         (0.229, 0.224, 0.225))])
+
+
+# Load vocabulary wrapper
+# with open('data_dir/vocabulary.pkl', 'rb') as f:
+#     vocabulary = pickle.load(f)
+
+with open(Basic_path+'vocabulary.pkl', 'rb') as f:
+    vocabulary = pickle.load(f)
+
+
+
+# Build models
+encoder_model = CNNModel(256).eval()  # eval mode (batchnorm uses moving mean/variance)
+decoder_model = LSTMModel(256, 512, len(vocabulary), 1)
+encoder_model = encoder_model.to(device)
+decoder_model = decoder_model.to(device)
+
+
+# Load the trained model parameters
+# encoder_model.load_state_dict(torch.load(Basic_path+'encoder-2-3000.ckpt'))
+# decoder_model.load_state_dict(torch.load(Basic_path+'decoder-2-3000.ckpt'))
+encoder_model.load_state_dict(torch.load(Basic_path+'encoder-4-3000.ckpt'))
+decoder_model.load_state_dict(torch.load(Basic_path+'decoder-4-3000.ckpt'))
+
+
+
+
+# Prepare an image
+img = load_image(image_file_path, transform)
+img_tensor = img.to(device)
+
+
+# Generate an caption from the image
+feat = encoder_model(img_tensor)
+sampled_indices = decoder_model.sample(feat)
+sampled_indices = sampled_indices[0].cpu().numpy()          # (1, max_seq_length) -> (max_seq_length)
+
+
+# Convert word_ids to words
+predicted_caption = []
+for token_index in sampled_indices:
+    word = vocabulary.i2w[token_index]
+    predicted_caption.append(word)
+    if word == '<end>':
+        break
+predicted_sentence = ' '.join(predicted_caption)
+
+
+# Print out the image and the generated caption
+%matplotlib inline
+print (predicted_sentence)
+img = Image.open(image_file_path)
+plt.imshow(np.asarray(img))
+
+
+
